d9392d2e09429e5927f076a9c19c3af99bef2194
taggatron <tagg_daniel@hotmail.com>
2025-10-25 22:37:30 +0100

    updated stylistic fixes

--- public/js/index.js
+++ public/js/index.js
@@ -631,9 +631,11 @@ function typeWriter(elementId, text, speed, callback) {
     if (callback) callback();
     return;
   }
+  // mark as typing to show a smaller blinking caret via CSS
+  targetEl.classList.add('typing');
   function type() {
     if (i < text.length) { targetEl.textContent += text.charAt(i); i++; setTimeout(type, speed); }
-    else { animating = false; if (callback) callback(); }
+    else { animating = false; targetEl.classList.remove('typing'); if (callback) callback(); }
   }
   type();
 }
